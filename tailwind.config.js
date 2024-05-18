6b4c33f023cd9ed50a98984b8c4377fe2c57f203
Reza <reza@Rezas-MacBook-Air.local>
2024-05-18 11:15:23 +0200

    nav-bar and first section of main

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    colors:{
+      'primay': '#2C7EF8',
+      'primary-hover-light': '#075CD9',
+      'on-primary-light':'#FFFFFF',
+  
+      'background-light': '#FFFFFF',
+      'on-background-light':'#191C1E',
+      'surface-light': '#FFFFFF',
+      'neutral_2': '#FEFEFE',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
