6892ebf8861100c5859664a4b11cd1686c2bc76a
Reza <reza@Rezas-MacBook-Air.local>
2024-05-19 13:53:04 +0200

    landingPage version 0

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,20 @@ module.exports = {
       'background-light': '#FFFFFF',
       'on-background-light':'#191C1E',
       'surface-light': '#FFFFFF',
+
+      
+      'neutral_1': '#FFFFFF',
       'neutral_2': '#FEFEFE',
+      'neutral_3': '#F5F6F8',
+      'neutral_4': '#E1E0E5',
+      'neutral_5': '#E1E0E5',
+      'neutral_6': '#C8C7CB',
+      'neutral_7': '#AFAEB2',
+      'neutral_8': '#7D7D7F',
+      'neutral_9': '#646466',
+      'neutral_9': '#323233',
+      'neutral_9': '#000000',
+
     },
     extend: {},
   },
